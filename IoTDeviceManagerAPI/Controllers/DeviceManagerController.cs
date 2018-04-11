3260d2eef1796bdaa8db9c416a83fd59a65f6bca
RaviTella <ravi.tella@outlook.com>
2018-04-11 17:46:34 -0500

    initial

---
+++ IoTDeviceManagerAPI/Controllers/DeviceManagerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Devices;
+using System.Text;
+using IoTDeviceManagerAPI.Model;
+
+namespace IoTDeviceManagerAPI.Controllers
+{
+     [Route("api/DeviceManager")]
+    public class DeviceManagerController : Controller
+    {
+        static ServiceClient serviceClient;
+        static string connectionString = "";
+
+        [HttpPost]         
+        public MessageContent CloudToDeviceMessage(MessageContent content)
+        {
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            SendCloudToDeviceMessageAsync(content).Wait();
+            return content;
+            
+        }
+
+        private async static Task SendCloudToDeviceMessageAsync(MessageContent content)
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes(content.MessageBody));
+            await serviceClient.SendAsync(content.DeviceID, commandMessage);
+        }
+    }
+}
---
+++ IoTDeviceManagerAPI/Model/MessageContent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoTDeviceManagerAPI.Model
+{
+    public class MessageContent
+    {
+        public virtual string DeviceID { get; set; }
+        public virtual string MessageBody { get; set; }
+    }
+}
